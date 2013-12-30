af1034401f0cbd6bb36ee85c255d0e9c20449948
Peter Mescalchin <peter@magnetikonline.com.au>
2013-12-30 14:32:14 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# PHP error and exception handler
+A very rudimentary error and exception handler for PHP 5.4 and above.
+
+- When caught, error/exception message is echoed back along with an easy to read stack trace.
+- New lines are converted to `<br />` for any [SAPI](http://www.php.net/php_sapi_name) type *other* than `cli`.
+- Execution is simply halted afterwards - PHP automatically [stops execution](http://www.php.net/manual/en/function.set-exception-handler.php) after Exceptions handled by `set_exception_handler()` anyway.
+- Future enhancements could include friendly error messages for end users in production and/or logging to file/datastore for caught errors. An exercise for another day, but for now this works well for my development.
+
+## Example
+```php
+<?php
+require('handler.php');
+
+function test() {
+
+	throw new Exception('This is an exception');
+	// trigger_error('This is an error');
+}
+
+test();
+```
+
+Output:
+
+	Exception: This is an exception
+
+	#0    Exception() at [/phperrorexceptionhandler/test.php:6]
+	#1    test() at [/phperrorexceptionhandler/test.php:10]
+
+## Further reading
+- [set_error_handler()](http://www.php.net/manual/en/function.set-error-handler.php)
+- [trigger_error()](http://www.php.net/trigger_error)
+- [set_exception_handler()](http://www.php.net/manual/en/function.set-exception-handler.php)
+- [Exception base class](http://www.php.net/manual/en/class.exception.php)
---
+++ handler.php
@@ -0,0 +1,68 @@
+<?php
+class ErrorExceptionHandler {
+
+	public static function error($errorNumber,$message) {
+
+		// skip first item, will be the call to static::error()
+		$stackTraceList = debug_backtrace();
+		array_shift($stackTraceList);
+
+		// build message and halt execution
+		static::buildMessage('Error',$message,$stackTraceList);
+		exit();
+	}
+
+	public static function exception(Exception $exception) {
+
+		// build complete exception stack trace
+		$stackTraceList = array_merge(
+			[[
+				'function' => 'Exception',
+				'file' => $exception->getFile(),
+				'line' => $exception->getLine()
+			]],
+			$exception->getTrace()
+		);
+
+		// build message - execution will halt at this point
+		static::buildMessage('Exception',$exception->getMessage(),$stackTraceList);
+	}
+
+	private static function buildMessage($type,$message,array $stackTraceList) {
+
+		// build error message and backtrace
+		$message =
+			sprintf("\n\n%s: %s\n\n",$type,$message) .
+			static::buildMessageBacktrace($stackTraceList) .
+			"\n\n";
+
+		// output, with <br /> if not in CLI mode
+		echo((PHP_SAPI == 'cli') ? $message : nl2br($message));
+	}
+
+	private static function buildMessageBacktrace(array $stackTraceList) {
+
+		// determine number of characters to cut from the start of file paths
+		$baseApplicationDirLength = strlen(dirname(__DIR__));
+		$messageList = [];
+		$stackCount = 0;
+
+		foreach ($stackTraceList as $stackTraceItem) {
+			// add message line
+			$messageList[] = sprintf(
+				'#%s %s%s() at [%s:%d]',
+				str_pad($stackCount++,4,' '), // stack trace item number, padded
+				(isset($stackTraceItem['type'])) ? $stackTraceItem['class'] . $stackTraceItem['type'] : '', // calling class details
+				$stackTraceItem['function'],
+				substr($stackTraceItem['file'],$baseApplicationDirLength), // strip redundant start of file path
+				$stackTraceItem['line'] // line number
+			);
+		}
+
+		return implode("\n",$messageList);
+	}
+}
+
+
+set_error_handler('ErrorExceptionHandler::error');
+set_exception_handler('ErrorExceptionHandler::exception');
